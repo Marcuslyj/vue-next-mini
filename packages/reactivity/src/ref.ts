645d4ae86f6adddb877dd815d2a46f46086a2294
Marcus <814712074@qq.com>
2023-05-12 15:56:56 +0800

    feat：ref 支持简单数据类型

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,5 +1,6 @@
+import { hasChanged } from '@vue/shared'
 import { createDep, Dep } from './dep'
-import { activeEffect, trackEffects } from './effect'
+import { activeEffect, trackEffects, triggerEffects } from './effect'
 import { toReactive } from './reactive'
 
 export interface Ref<T = any> {
@@ -20,19 +21,33 @@ function createRef(rawValue: unknown, shallow: boolean) {
 
 class RefImpl<T> {
   private _value: T
+  private _rawValue: T
   public dep?: Dep = undefined
   public readonly __v_isRef = true
 
   constructor(value: T, public readonly __v_isShallow: boolean) {
     this._value = __v_isShallow ? value : toReactive(value)
+    this._rawValue = value
   }
-
+  // 简单数据类型通过 trackRefValue & triggerRefValue实现响应式
   get value() {
-    // trackRefValue(this)
+    trackRefValue(this)
     return this._value
   }
 
-  set value(newValue) {}
+  set value(newValue) {
+    if (hasChanged(newValue, this._rawValue)) {
+      this._rawValue = newValue
+      this._value = toReactive(newValue)
+      triggerRefValue(this)
+    }
+  }
+}
+
+export function triggerRefValue(ref: RefImpl<unknown>) {
+  if (ref.dep) {
+    triggerEffects(ref.dep)
+  }
 }
 
 function isRef(r: any): r is Ref {
