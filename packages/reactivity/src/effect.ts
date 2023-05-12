5468cd2fd309644456e54701dd4e2609dc466013
Marcus <814712074@qq.com>
2023-05-12 09:42:03 +0800

    feat: 依赖收集，建立数据和副作用函数的关联关系

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,3 +1,6 @@
+type KeyToDepMap = Map<any, ReactiveEffect>
+const targetMap = new WeakMap<any, KeyToDepMap>()
+
 export function effect<T = any>(fn: () => T) {
   const _effect = new ReactiveEffect(fn)
   _effect.run()
@@ -22,6 +25,12 @@ export class ReactiveEffect<T = any> {
  */
 export function track(target: object, key: unknown) {
   console.log('track：收集依赖')
+  if (!activeEffect) return
+
+  let depsMap = targetMap.get(target)
+  if (!depsMap) targetMap.set(target, (depsMap = new Map()))
+
+  depsMap.set(key, activeEffect)
 }
 
 /**
