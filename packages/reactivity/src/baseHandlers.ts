6a33e7a60e2fc801eb0a14250c3897612d686519
Marcus <814712074@qq.com>
2023-08-31 17:25:42 +0800

    feat: getter setter

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,1 +1,32 @@
-export const mutableHandlers: ProxyHandler<object> = {};
+const get = createGetter();
+function createGetter() {
+  return function get(target: any, key: string | symbol, receiver: object) {
+    // get值
+    const res = Reflect.get(target, key, receiver);
+
+    //  TODO：收集依赖
+    // track(target, key);
+
+    return res;
+  };
+}
+
+const set = createSetter();
+function createSetter() {
+  return function set(
+    target: object,
+    key: string | symbol,
+    value: unknown,
+    receiver: object
+  ) {
+    // set值
+    const result = Reflect.set(target, key, value, receiver);
+
+    //  TODO：触发副作用函数
+    // // trigger(target, key,value);
+
+    return result;
+  };
+}
+
+export const mutableHandlers: ProxyHandler<object> = { get, set };
