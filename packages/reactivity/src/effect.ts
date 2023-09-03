d05b619b18ad28ece9fe1706df60e1eb6a417d45
Marcus <814712074@qq.com>
2023-09-03 21:18:12 +0800

    feat: effect 函数

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,3 +1,19 @@
+export function effect<T = any>(fn: () => T) {
+  const _effect = new ReactiveEffect(fn);
+  // 完成第一次 fn 函数的执行
+  _effect.run();
+}
+
+export let activeEffect: ReactiveEffect | undefined;
+export class ReactiveEffect<T = any> {
+  constructor(public fn: () => T) {}
+
+  run() {
+    activeEffect = this;
+    return this.fn();
+  }
+}
+
 /**
  * 收集依赖（建立被依赖数据和副作用函数的关联关系）
  * @param target
