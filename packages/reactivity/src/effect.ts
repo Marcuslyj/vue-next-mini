3d0053f9da37b4a5534eb5bf200125de55f5cede
Marcus <814712074@qq.com>
2023-09-05 21:13:51 +0800

    feat: targetMap的数据接口定义& track trigger的简单实现

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,3 +1,13 @@
+type KeyToDepMap = Map<any, ReactiveEffect>;
+/**
+ * key: 响应式对象
+ * value：Map 对象{
+ *  key: 响应式对象的指定属性
+ *  value：指定对象的指定属性的执行函数
+ * }
+ */
+const targetMap = new WeakMap<any, KeyToDepMap>();
+
 export function effect<T = any>(fn: () => T) {
   const _effect = new ReactiveEffect(fn);
   // 完成第一次 fn 函数的执行
@@ -9,20 +19,38 @@ export class ReactiveEffect<T = any> {
   constructor(public fn: () => T) {}
 
   run() {
+    // 全局定义当前激活的 ReactiveEffect 实例
     activeEffect = this;
+    // 执行 fn 会触发 getter
     return this.fn();
   }
 }
 
 /**
  * 收集依赖（建立被依赖数据和副作用函数的关联关系）
+ * 需要知道这个fn 函数是哪个响应式数据的哪个属性触发的
  * @param target
  * @param key
  */
-export function track(target: string, key: unknown) {
+export function track(target: object, key: unknown) {
   console.log('收集依赖', target, key);
+  if (!activeEffect) return;
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()));
+  }
+
+  depsMap.set(key, activeEffect);
+  console.log(targetMap);
 }
 
 export function trigger(target: object, key: unknown, newValue: unknown) {
   console.log('触发更新', target, key, newValue);
+  const depsMap = targetMap.get(target);
+  if (!depsMap) return;
+
+  const effect = depsMap.get(key) as ReactiveEffect;
+  if (!effect) return;
+
+  effect.run();
 }
