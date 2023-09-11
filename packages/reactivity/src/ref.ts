97181d49ef1db6dad2159f6045323879215b4fb3
Marcus <814712074@qq.com>
2023-09-11 17:23:38 +0800

    feat: ref 实现简单数据类型的响应式

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,5 +1,6 @@
+import { hasChanged } from '@vue/shared';
 import { createDep, Dep } from './dep';
-import { activeEffect, trackEffects } from './effect';
+import { activeEffect, trackEffects, triggerEffects } from './effect';
 import { toReactive } from './reactive';
 
 export interface Ref<T = any> {
@@ -23,25 +24,45 @@ export function isRef(r: any): r is Ref {
 
 class RefImpl<T> {
   private _value: T;
+  private _rawValue: T;
   public dep?: Dep = undefined;
   public readonly __v_isRef = true;
 
   constructor(value: T, public readonly __v_isShallow: boolean) {
+    this._rawValue = value;
     // __v_isShallow理解成简单数据类型
     this._value = __v_isShallow ? value : toReactive(value);
   }
 
   get value() {
     // 收集依赖
-    trackRefValue(this);
+    trackRefValue(this); // 复杂数据类型为什么需要这个操作？
     return this._value;
   }
 
-  set value(newVal) {}
+  set value(newVal) {
+    if (hasChanged(newVal, this._rawValue)) {
+      this._rawValue = newVal;
+      this._value = toReactive(newVal);
+      triggerRefValue(this);
+    }
+  }
 }
 
+/**
+ * 收集依赖
+ */
 function trackRefValue(ref: RefImpl<any>) {
   if (activeEffect) {
     trackEffects(ref.dep || (ref.dep = createDep()));
+  }
+}
+
+/**
+ * 触发依赖
+ */
+function triggerRefValue(ref: RefImpl<any>) {
+  if (ref.dep) {
+    triggerEffects(ref.dep);
   }
 }
