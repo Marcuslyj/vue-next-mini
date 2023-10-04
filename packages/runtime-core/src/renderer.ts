f319744a14b95675896dec70140ced2201a099f7
Marcus <814712074@qq.com>
2023-10-04 21:20:48 +0800

    feat:基于 renderer 完成 element节点挂载

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -5,14 +5,7 @@ export interface RendererOptions {
   /**
    * 为指定的 element 的 props 打补丁
    */
-  patchProp(
-    el: Element,
-    key: string,
-    prevVal: any,
-    nextVal: any,
-    prevValue: any,
-    nextValue: any
-  ): void;
+  patchProp(el: Element, key: string, prevValue: any, nextValue: any): void;
   /**
    * 为指定的 element 设置 text
    */
@@ -26,6 +19,40 @@ export function createRenderer(options: RendererOptions) {
 }
 
 function baseCreateRenderer(options: RendererOptions) {
+  const {
+    insert: hostInsert,
+    patchProp: hostPatchProp,
+    createElement: hostCreateElement,
+    setElementText: hostSetElementText
+  } = options;
+
+  const processElement = (oldValue, newVNode, container, anchor) => {
+    if (oldValue == null) {
+      mountElement(newVNode, container, anchor);
+    } else {
+      // todo:更新操作
+    }
+  };
+
+  const mountElement = (vnode, container, anchor) => {
+    const { type, props, shapeFlag } = vnode;
+    // 1.创建 element
+    const el = (vnode.el = hostCreateElement(type));
+    // 2.设置文本
+    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+      hostSetElementText(el, vnode.children);
+    } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+    }
+    // 3.设置 props
+    if (props) {
+      for (const key in props) {
+        hostPatchProp(el, key, null, props[key]);
+      }
+    }
+    // 4.插入
+    hostInsert(el, container, anchor);
+  };
+
   const patch = (oldVNode, newVNode, container, anchor?) => {
     if (oldVNode === newVNode) {
       return;
@@ -42,6 +69,7 @@ function baseCreateRenderer(options: RendererOptions) {
         break;
       default:
         if (shapeFlag & ShapeFlags.ELEMENT) {
+          processElement(oldVNode, newVNode, container, anchor);
         } else if (shapeFlag & ShapeFlags.COMPONENT) {
         }
     }
